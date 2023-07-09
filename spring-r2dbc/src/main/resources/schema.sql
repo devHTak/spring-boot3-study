04ccc41ab9bbcf807589f8c9daf82599f1606550
devHTak <qkrgusxkr1@gmail.com>
2023-07-09 18:06:50 +0900

    # R2DBC relational study

--- spring-r2dbc/src/main/resources/schema.sql
+++ spring-r2dbc/src/main/resources/schema.sql
@@ -16,3 +16,10 @@ create table orders (
     item_id BIGINT NOT NULL,
     created_at datetime NOT NULL
 );
+
+create table relational_orders (
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    member_id BIGINT NOT NULL,
+    item_id BIGINT NOT NULL,
+    created_at datetime NOT NULL
+);
